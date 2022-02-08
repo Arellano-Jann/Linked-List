dc69b774604db623b6a0198e64c5878d8f08a2b0
Arellano Jann <89598304+Arellano-Jann@users.noreply.github.com>
2022-02-08 11:00:22 -0800

    Added some questionable stuff

--- LinkedList.h
+++ LinkedList.h
@@ -4,3 +4,18 @@
 // remove all tasks
 // exit program/to do list
 
+#pragma once
+#define LINKEDLIST_H
+
+template <typename T>
+class LinkedList{
+    T item;
+    LinkedList<T>* next;
+
+public:
+    LinkedList();
+    void setItem(const T& anItem);
+    void setNext(LinkedList<T>* nextPtr);
+
+    
+}
